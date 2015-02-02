7ab756470d768945940914de08009dac9472b399
agoryu <vang158@live.fr>
2015-02-02 10:55:52 +0100

    modification des contextes

--- src/hw/hw.c
+++ src/hw/hw.c
@@ -7,63 +7,15 @@
    
 */
 
-#include <sys/time.h>
-#include <signal.h>
 #include <stdio.h>
-#include <assert.h>
 
 #include "hw/hw.h"
 #include "hw/hardware.h"
 #include "context/context.h"
 
-/* a 8 milliseconds timer */
-#define TIMER_SEC  0
-#define TIMER_USEC 8000
-
-/*static irq_handler_func_t *timer_irq_handler = (irq_handler_func_t *) 0;
-static int irqs_are_enable = 1;*/
-
-/*static void empty_it(){
+static void empty_it(){
   return;
-}*/
-
-/*static void do_timer_interrupt() {
-
-  if (timer_irq_handler && irqs_are_enable) {
-    timer_irq_handler();
-  }
-	  
-}*/
-
-/*void start_hw(){
-
-  {*/
-    /* start timer handler */
-    /*static struct sigaction sa;
-	
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = do_timer_interrupt;
-    sa.sa_flags = SA_RESTART | SA_NODEFER;
-    sigaction(SIGALRM, &sa, (struct sigaction *)0);
-  }
-
-  {*/
-    /* set timer */ 
-    /*struct itimerval value;*/
-
-    /* timer period */
-    /*value.it_interval.tv_sec = TIMER_SEC;
-    value.it_interval.tv_usec = TIMER_USEC;*/
-    /* first deliverable */
-    /*value.it_value = value.it_interval;
-    setitimer(ITIMER_REAL, &value, (struct itimerval *)0);
-  }
-}*/
-
-/*void setup_irq(unsigned int irq, irq_handler_func_t handler) {
-  assert(irq == TIMER_IRQ);
-  timer_irq_handler = handler;
-}*/
+}
 
 void irq_disable() {
   _mask(15);
@@ -72,7 +24,6 @@ void irq_disable() {
 void irq_enable() {
   _mask(1);
 }
-
 
 unsigned boot() {
 
@@ -84,9 +35,10 @@ unsigned boot() {
   }
 
   /* Interreupt handlers */
-  /*for(i=0; i<16; i++){
+  for(i=0; i<16; i++){
     IRQVECTOR[i] = empty_it;
-  }*/
+  }
+
   IRQVECTOR[TIMER_IRQ] = yield();
 
   /* Allows all IT */
